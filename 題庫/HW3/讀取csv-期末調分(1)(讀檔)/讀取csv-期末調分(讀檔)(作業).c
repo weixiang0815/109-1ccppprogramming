921655552e68aa9d344ac0ba6cd55cc466a2386c
weixiang0815 <79576059+weixiang0815@users.noreply.github.com>
2021-03-10 21:03:44 +0800

    Add files via upload

---
+++ 題庫/HW3/讀取csv-期末調分(1)(讀檔)/讀取csv-期末調分(讀檔)(作業).c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+#include<math.h>
+int main(){
+	int score[1000],id,i=0,sum=0;
+	char n[200]="./",key[200];
+	memset(key,'\0',200);
+	scanf("%s",key);
+	strcat(n,key);
+	FILE *f=fopen(n,"r");
+	fscanf(f,"%*[^\n]");
+	fscanf(f,"%*c");
+	while(1){
+		fscanf(f,"%d,%d\n",&id,&score[i]);
+		score[i]=(int)(sqrt((double)score[i])*11);
+		if(score[i]>100){
+			score[i]=100;
+		}
+		sum+=score[i];
+		i++;
+		if(feof(f)){
+			break;
+		}
+	}
+	fclose(f);
+	double avg=(double)sum/i;
+	printf("%.1lf",avg);
+	return 0;
+}
